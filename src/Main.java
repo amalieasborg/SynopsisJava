7dedfdc2df92eb42a239258f98aa77dfad450ed7
amalieasborg <amalieasborg@gmail.com>
2025-05-27 09:56:50 +0200

    Indhold

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer i = new Integer(4);
+        //Det nye Integer-objekt er tilgængelig via referencen i 'i'
+        i = null;
+        //Integer objektet er ikke længere tilgængelig
+        System.out.println(i);
     }
 }
+
+
